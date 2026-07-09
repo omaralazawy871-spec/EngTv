690e435ba0fa9ffbd34c34831786b7dc35e87116
omaralazawy871-spec <omaralazawy871@gmail.com>
2026-07-09 08:15:08 +0300

    Update IptvViewModel.kt

--- app/src/main/java/com/example/ui/IptvViewModel.kt
+++ app/src/main/java/com/example/ui/IptvViewModel.kt
@@ -182,5 +182,16 @@ class IptvViewModel(
 
     }
 
+    fun updatePlaylists() {
 
+        viewModelScope.launch {
+
+            repository.addPlaylistFromUrl(
+                name = "ALWAN VIP",
+                url = "https://m3uextractor.indexiptv212.workers.dev/download/0241cbac-61a4-4b8e-9973-1c289d2232fc/Live_AR%20_%20ALWAN%20VIP.m3u8"
+            )
+
+        }
+
+    }
 }
