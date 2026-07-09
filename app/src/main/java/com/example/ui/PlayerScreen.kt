0935753d2180de5d7c6962b933527a28d0252a0a
omaralazawy871-spec <omaralazawy871@gmail.com>
2026-07-09 07:47:34 +0300

    Update PlayerScreen.kt

--- app/src/main/java/com/example/ui/PlayerScreen.kt
+++ app/src/main/java/com/example/ui/PlayerScreen.kt
@@ -1,1 +1,56 @@
+package com.example.ui
 
+import android.net.Uri
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import com.example.data.Channel
+
+
+@Composable
+fun PlayerScreen(
+    channel: Channel,
+    onBack: () -> Unit
+) {
+
+    val context = LocalContext.current
+
+    val player = remember {
+
+        ExoPlayer.Builder(context)
+            .build()
+            .apply {
+
+                setMediaItem(
+                    MediaItem.fromUri(
+                        Uri.parse(channel.streamUrl)
+                    )
+                )
+
+                prepare()
+                play()
+
+            }
+
+    }
+
+
+    AndroidView(
+        factory = {
+
+            PlayerView(it).apply {
+
+                this.player = player
+
+            }
+
+        },
+        modifier = Modifier
+    )
+
+}
